c92322f74c3f1bbc795e216e3ec17aeac054b7cb
mox <de_mox@zoho.com>
2014-03-18 17:27:09 +0100

    some improvements to the 7 segment controller, corrected the sequence of the digits in the ucf file

--- a43a/a43.v
+++ a43a/a43.v
@@ -1,38 +1,20 @@
 module a43(input clk, input [7:0] switches, output [7:0] seg, output [3:0] an);
 
 wire [15:0] full_count;
-reg [4:0] numbers [0:3];
+reg [3:0] numbers [0:3];
+reg [3:0] points;
 counter #(.width(16)) my_counter(.clk(clk), .increment(switches), .count(full_count));
 
-seven_seg_controller my_display(.clk(full_count[15]), .digits0(numbers[0]), .digits1(numbers[1]), .digits2(numbers[2]), .digits3(numbers[3]), .segments({dp,seg}), .anodes(an));
+seven_seg_controller my_display(.clk(full_count[15]), .digits({numbers[3], numbers[2], numbers[1], numbers[0]}), .decimal_points(points), .segments({dp,seg}), .anodes(an));
 
 initial
 begin
-	numbers[0] = {0, 4'h0};
-	numbers[1] = {0, 4'h1};
-	numbers[2] = {0, 4'h2};
-	numbers[3] = {1, 4'h3};
+	numbers[0] = {4'ha};
+	numbers[1] = {4'hb};
+	numbers[2] = {4'hc};
+	numbers[3] = {4'hd};
+
+	points = 4'b1111;
 end
 
 endmodule
-
-
-
-
-//	seven_seg_decoder my_decoder(.digit(0), .segments(seg));
-//
-//	assign an = 4'b0111;
-//	assign dp = 1;
-//	//assign seg = 7'b1000000;
-//
-//endmodule
-
-
-
-
-
-
-
-
-
-
--- a43a/seven_seg_controller.v
+++ a43a/seven_seg_controller.v
@@ -1,23 +1,24 @@
 module seven_seg_controller (input clk,
-                             input [4:0] digits0,
-                             input [4:0] digits1,
-                             input [4:0] digits2,
-                             input [4:0] digits3,
+                             input [15:0] digits,
+                             input [3:0] decimal_points,
 							 output [7:0] segments,
 							 output reg[3:0] anodes);
 
-	// For some silly reason verilog will not allow arrays as ports so this
-	// limbo is nessesary
-	wire [4:0] digits [0:3]; // Using an Array allows Multiplexing by index
-	assign digits[0] = digits0; assign digits[1] = digits1; assign digits[2] = digits2; assign digits[3] = digits3;
+	// For some silly reason verilog will not allow arrays as ports so this limbo is nessesary
+	wire [4:0] digits_array [0:3]; // Using an Array allows Multiplexing by index
+	assign digits_array[3] = digits[15:12];
+	assign digits_array[2] = digits[11:8];
+	assign digits_array[1] = digits[7:4];
+	assign digits_array[0] = digits[3:0];
 
 	wire [1:0] digit_index; // Which digit to drive
 	counter #(.width(2)) my_counter(.clk(clk), .increment(1), .count(digit_index));
-
 	wire [3:0] digit;
+	assign digit = digits_array[digit_index];
+	assign segments[7] = decimal_points[digit_index];
+
 	seven_seg_decoder mydecoder(.digit(digit), .segs(segments[6:0]));
 
-	assign {segments[7], digit} = digits[digit_index]; // segments[7] is the decimal point, which does not need any decoding
 
 	always @(posedge clk)
 	begin
